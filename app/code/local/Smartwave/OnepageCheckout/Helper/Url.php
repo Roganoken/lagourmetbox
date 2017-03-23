0985d093e67eb78d5613ca78a025493b2c785e13
vincent <vincent@vincent-linux>
2017-03-23 17:33:14 +0900

    Adjust default checkout and add styles

--- app/code/local/Smartwave/OnepageCheckout/Helper/Url.php
+++ app/code/local/Smartwave/OnepageCheckout/Helper/Url.php
@@ -3,6 +3,12 @@ class Smartwave_OnepageCheckout_Helper_Url extends Mage_Checkout_Helper_Url
 {
     public function getCheckoutUrl()
     {
-        return $this->_getUrl('onepagecheckout', array('_secure'=>true));
+        if (Mage::helper('onepagecheckout')->isOnepageCheckoutEnabled()) {
+            $url = $this->_getUrl('onepagecheckout', array('_secure'=>true));
+        } else {
+            $url = $this->_getUrl('checkout/onepage', array('_secure'=>true));
+        }
+        
+        return $url;
     }
 }
