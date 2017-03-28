b75b3c5a3c40de36905aaa79e1edceece3ad28de
vincent <vincent@vincent-linux>
2017-03-28 14:49:18 +0900

    fix BO legenda error CSS

--- app/code/local/Smartwave/Legenda/Model/Observer.php
+++ app/code/local/Smartwave/Legenda/Model/Observer.php
@@ -7,23 +7,12 @@ class Smartwave_Legenda_Model_Observer
     // After any system config is saved
     public function legenda_controllerActionPostdispatchAdminhtmlSystemConfigSave()
     {
-        $section = Mage::app()->getRequest()->getParam('section');
-        
-        $websiteCode = Mage::app()->getRequest()->getParam('website');
-        $storeCode = Mage::app()->getRequest()->getParam('store');
-        
-        Mage::getSingleton('legenda/config_generator')->generateCss('setting',$websiteCode, $storeCode);
-        Mage::getSingleton('legenda/config_generator')->generateCss('design',$websiteCode, $storeCode);
+        return $this;
     }
     
     // After store view is saved
     public function legenda_storeEdit(Varien_Event_Observer $observer)
     {
-        $store = $observer->getEvent()->getStore();
-        $storeCode = $store->getCode();
-        $websiteCode = $store->getWebsite()->getCode();
-        
-        Mage::getSingleton('legenda/config_generator')->generateCss('setting',$websiteCode, $storeCode);
-        Mage::getSingleton('legenda/config_generator')->generateCss('design',$websiteCode, $storeCode);
+        return $this;
     }
 }
